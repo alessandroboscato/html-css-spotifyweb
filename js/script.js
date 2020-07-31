7c2c92e2732f19cfe28d6261d0b682bf3dc69147
Alessandro <alessandro.boscato@gmail.com>
2020-07-31 16:41:02 +0200

    Tentativo 2 (fallito)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 alert("Questo account ha effettuato l'upgrade alla versione premium");
 
-document.getElementById("upgrade").innerHTML = "UPGRADE EFFETTUATO";
+// document.getElementById("upgrade").innerHTML = "UPGRADE EFFETTUATO";
+
+// function upgradeEffettuata() {
+// document.getElementById("upgrade").innerHTML = "UPGRADE EFFETTUATO";
+// } NON FUNZIONA
+
+document.getElementById("signIn").onclick = function() {upgradeDone()};
+
+function upgradeDone() {
+  document.getElementById("signIn").innerHTML = "UPGRADE EFFETTUATO";
