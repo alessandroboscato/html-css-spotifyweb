ef44431162c485891f0768046af18d4da936dc6f
Alessandro <alessandro.boscato@gmail.com>
2020-08-03 17:44:09 +0200

    Inizio js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-alert("Questo account ha effettuato l'upgrade alla versione premium");
+alert("Ciao Betta!");
 
-// document.getElementById("upgrade").innerHTML = "UPGRADE EFFETTUATO";
-
-// function upgradeEffettuata() {
-// document.getElementById("upgrade").innerHTML = "UPGRADE EFFETTUATO";
-// } NON FUNZIONA
-
-document.getElementById("signIn").onclick = function() {upgradeDone()};
-
-function upgradeDone() {
-  document.getElementById("signIn").innerHTML = "UPGRADE EFFETTUATO";
+document.getElementById("upgrade").innerHTML = "UPGRADE EFFETTUATO";
